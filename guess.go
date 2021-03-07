09d947f3c3757c5a0c948e99a892303548478191
hgfgdsy <1031010120@qq.com>
2021-03-07 17:41:00 +0800

    with

--- guess.go
+++ guess.go
@@ -3,9 +3,11 @@ package firstlibrary
 import (
 	"fmt"
 	"github.com/hgfgdsy/firstlibrary/subl"
+	"github.com/hgfgdsy/alix"
 )
 
 func Greetings() {
 	fmt.Println("Hello, world111!")
 	subl.Goodybe()
+	alix.Nice()
 }
